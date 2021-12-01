71bb053a816eaefcdc24aa2de7ba6d0896a38a02
kosarev-sa <kosarev-sa@mail.ru>
2021-12-02 01:41:34 +0300

    + tasks_lesson6

--- todo/users/admin.py
+++ todo/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import User
 
 # Register your models here.
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     model = User
+
+
+admin.site.register(User, UserAdmin)
