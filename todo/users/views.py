c3d58d5b9bf92bfdc6db3cf18d6ad6614152da04
kosarev-sa <kosarev-sa@mail.ru>
2021-12-14 11:28:14 +0300

    + tasks_lesson8

--- todo/users/views.py
+++ todo/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .serializers import UserModelSerializer
@@ -6,6 +6,6 @@ from .models import User
 
 # Create your views here.
 
-class UserModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
    queryset = User.objects.all()
    serializer_class = UserModelSerializer
